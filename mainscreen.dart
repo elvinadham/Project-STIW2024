90866b168b06d9980c0df63ddf31bd05611a06b3
Elvin Adham <38040717+elvinadham@users.noreply.github.com>
2020-01-15 13:04:02 +0800

    Add files via upload

---
+++ mainscreen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:admin_foodnowaste/tab_screen2.dart';
+import 'package:admin_foodnowaste/tab_screen3.dart';
+import 'package:admin_foodnowaste/tab_screen4.dart';
+import 'package:admin_foodnowaste/user.dart';
+import 'tab_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  final User user;
+
+  const MainScreen({Key key, this.user}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<Widget> tabs;
+
+  int currentTabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    tabs = [
+      TabScreen(user: widget.user),
+      TabScreen2(user: widget.user),
+      TabScreen3(user: widget.user),
+      TabScreen4(user: widget.user),
+    ];
+  }
+
+  String $pagetitle = "Admin Food No Waste";
+
+  onTapped(int index) {
+    setState(() {
+      currentTabIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.lightGreen));
+    return Scaffold(
+      body: tabs[currentTabIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTapped,
+        currentIndex: currentTabIndex,
+        //backgroundColor: Colors.blueGrey,
+        type: BottomNavigationBarType.fixed,
+
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Available Food"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood, ),
+            title: Text("Posted Food"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.filter_vintage, ),
+            title: Text("Donate/sell"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, ),
+            title: Text("Profile"),
+          )
+        ],
+      ),
+    );
+  }
+}
